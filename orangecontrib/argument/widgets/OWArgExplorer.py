ba75916dbc996469df3114d10f3d50a5f9c6ad5e
Ji Qi <92043159+jiqicn@users.noreply.github.com>
2023-01-19 16:43:50 +0100

    Start framing the explorer widget.
    Add output of miner widget as input; reuse the graphview module from orange3-network add-on; change the input into required format; setup a initial view with random positions of the nodes; still have issue with pen width, need to check the setup in the graphview module.

---
+++ orangecontrib/argument/widgets/OWArgExplorer.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+from Orange.data import Table
+from Orange.widgets.widget import Input
+from Orange.widgets.visualize.utils.widget import OWDataProjectionWidget
+from Orange.widgets.settings import SettingProvider
+from Orange.data.pandas_compat import table_to_frame
+
+from orangecontrib.argument.graph.graphview import GraphView
+
+class OWArgExplorer(OWDataProjectionWidget):
+    name = 'Argument Explorer'
+    description = 'Visually explore arguments in their attacking network.'
+    icon = 'icons/OWArgExplorer.svg'
+    
+    class Inputs:
+        edge_data = Input('Edge Data', Table)
+        node_data = Input('Node Data', Table)
+        
+    GRAPH_CLASS = GraphView # borrowed from Orange3-network add-on
+    graph = SettingProvider(GraphView) 
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.edge_data = None
+        self.node_data = None
+        self.positions = None
+        
+    @Inputs.edge_data
+    def set_edge_data(self, data):
+        self.edge_data = data
+        
+    @Inputs.node_data
+    def set_node_data(self, data):
+        self.node_data = data
+        
+    def handleNewSignals(self):
+        self.relayout()
+        
+    def relayout(self):
+        if self.positions is None:
+            self.set_random_positions()
+        
+        self.closeContext()
+        self.data = self.node_data
+        self.valid_data = np.full(len(self.data), True, dtype=bool)
+        self.openContext(self.data)
+        
+        self.graph.reset_graph()
+        self.graph.update_coordinates()
+            
+    def set_random_positions(self):
+        if self.node_data is not None:
+            num_nodes = len(self.node_data)
+            self.positions = np.random.uniform(size=(num_nodes, 2))
+            
+    def get_embedding(self):
+        return self.positions
+    
+    def get_edges(self):
+        return table_to_frame(self.edge_data)
