4ae4109df2693a473497cfc1c8c5c9369befbeae
jiqicn <qiji1988ben@gmail.com>
2023-06-01 16:24:59 +0200

    mend

--- orangecontrib/argument/miner/topic.py
+++ orangecontrib/argument/miner/topic.py
@@ -18,6 +18,8 @@ import numpy as np
 from textblob import TextBlob
 
 class ArgumentChunker:
+    """Split arguments into smaller but meaningful chunks.
+    """
     def __init__(self, docs:List[str]):
         self.docs = docs
         self.df_chunks = None
@@ -52,7 +54,9 @@ class ArgumentChunker:
                 argt_ids.append(i)
                 chunks.append(chunk)
         
-        self.df_chunks = pd.DataFrame({'argument_id': argt_ids, 'chunk': chunks})
+        self.df_chunks = pd.DataFrame(
+            {'argument_id': argt_ids, 'chunk': chunks}
+        )
     
     def chunk_polarity_score(self):
         """Compute chunk polarity scores.
@@ -96,55 +100,24 @@ class ArgumentChunker:
         self.chunk_polarity_score()
         return copy.deepcopy(self.df_chunks)
     
-
-# def merge_chunks(docs:List[str], doc_ids:List[int], topics:List[int], \
-#     chunk_ranks:list[float], chunk_polarity_scores:List[float], \
-#         topic_keywords:dict, n_keywords:int):
-#     """For each argument, merge topics of chunks into one, in format of keyword and importance.
-
-#     Args:
-#         docs (List[str]): argument doc list.
-#         doc_ids (list[int]): chunk's doc ids.
-#         topics (list[int]): chunk's topic ids.
-#         chunk_ranks: list[float]: chunkrank in chunk corpus.
-#         chunk_polarity_scores: list[float]: chunk polarity scores.
-#         topic_keywords (dict): all topic keywords and importance.
-#     """
-#     docs = pd.DataFrame({"doc": docs}) 
-#     chunks = pd.DataFrame({
-#         "doc_id": doc_ids, 
-#         "topic": topics
-#     })
+    def get_topic_table(self):
+        """Get topic info table that includes the index, name, count of 
+        corresponding chunks, keywords, and scores.
+        """
+        assert "topic" in self.df_chunks.columns, \
+            "Should do topic modeling before getting topic info table!"
+        self.topic_model.get_topic_table()
     
-#     def get_keywords_scores(topics:List[int]):
-#         keywords = [topic_keywords[t] for t in topics if t != -1]
-#         keywords = list(itertools.chain.from_iterable(keywords))
-#         keywords = pd.DataFrame({
-#             "keyword": [kw[0] for kw in keywords], 
-#             "keyword_scores": [kw[1] for kw in keywords]
-#         }) 
-#         keywords = keywords.groupby("keyword", as_index=False).\
-#             sum().sort_values(by="keyword_scores", ascending=False).reset_index(drop=True)
-#         keywords = keywords.loc[0:n_keywords-1]
-#         return keywords["keyword"].tolist(), keywords["keyword_scores"].tolist()
-    
-#     chunks["topic"] = chunks["topic"].apply(lambda x: [x])
-#     docs["topic"] = chunks.groupby("doc_id", as_index=False).agg("sum")["topic"]
-#     temp = docs["topic"].apply(get_keywords_scores)
-#     docs[["keyword", "keyword_scores"]] = pd.DataFrame(temp.tolist(), index=temp.index)
-    
-#     return docs
-
 class ArgumentTopic(BERTopic):
-    
+    """
+    BERT-based topic modeling and doc ranking.
+    """
     def __init__(self):
         self.embedding_model = SentenceTransformer("all-mpnet-base-v1")
         self.umap_model = UMAP(n_components=5)
         self.hdbscan_model = HDBSCAN(min_cluster_size=10, prediction_data=True)
         self.vectorizer_model = CountVectorizer(stop_words='english', ngram_range=[1, 1])
         self.ctfidf_model = ClassTfidfTransformer(reduce_frequent_words=True)
-        
-        # NOTE: The POS patterns should be discussed further.
         self.representation_model = PartOfSpeech("en_core_web_md", pos_patterns=[
             [{"POS": "NOUN"}], 
             [{"POS": "ADJ"}], 
@@ -212,3 +185,41 @@ class ArgumentTopic(BERTopic):
         ranks = np.array(ranks)
         ranks = ranks / ranks.max() # max normalization
         return ranks
+    
+    
+# # HACK: not sure we will need to merge chunks back, just leave it for now.
+# def merge_chunks(docs:List[str], doc_ids:List[int], topics:List[int], \
+#     chunk_ranks:list[float], chunk_polarity_scores:List[float], \
+#         topic_keywords:dict, n_keywords:int):
+#     """For each argument, merge topics of chunks into one, in format of keyword and importance.
+#         docs (List[str]): argument doc list.
+#         doc_ids (list[int]): chunk's doc ids.
+#         topics (list[int]): chunk's topic ids.
+#         chunk_ranks: list[float]: chunkrank in chunk corpus.
+#         chunk_polarity_scores: list[float]: chunk polarity scores.
+#         topic_keywords (dict): all topic keywords and importance.
+#     """
+#     docs = pd.DataFrame({"doc": docs}) 
+#     chunks = pd.DataFrame({
+#         "doc_id": doc_ids, 
+#         "topic": topics
+#     })
+    
+#     def get_keywords_scores(topics:List[int]):
+#         keywords = [topic_keywords[t] for t in topics if t != -1]
+#         keywords = list(itertools.chain.from_iterable(keywords))
+#         keywords = pd.DataFrame({
+#             "keyword": [kw[0] for kw in keywords], 
+#             "keyword_scores": [kw[1] for kw in keywords]
+#         }) 
+#         keywords = keywords.groupby("keyword", as_index=False).\
+#             sum().sort_values(by="keyword_scores", ascending=False).reset_index(drop=True)
+#         keywords = keywords.loc[0:n_keywords-1]
+#         return keywords["keyword"].tolist(), keywords["keyword_scores"].tolist()
+    
+#     chunks["topic"] = chunks["topic"].apply(lambda x: [x])
+#     docs["topic"] = chunks.groupby("doc_id", as_index=False).agg("sum")["topic"]
+#     temp = docs["topic"].apply(get_keywords_scores)
+#     docs[["keyword", "keyword_scores"]] = pd.DataFrame(temp.tolist(), index=temp.index)
+    
+#     return docs
