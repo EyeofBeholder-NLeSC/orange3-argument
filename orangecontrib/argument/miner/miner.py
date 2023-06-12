b52c2cc8575499e2ab1b82d0394bd896cbc54261
Ji Qi <92043159+jiqicn@users.noreply.github.com>
2023-06-12 17:48:57 +0200

    Implement the ArgumentMiner class.

--- orangecontrib/argument/miner/miner.py
+++ orangecontrib/argument/miner/miner.py
@@ -11,241 +11,78 @@ from itertools import starmap, combinations
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from typing import Any, Callable, Tuple
+import copy
+import itertools
 
 
 class ArgumentMiner:
-    def __init__(self, df_arguments, df_chunks):
+    def __init__(self, df_arguments):
         self.df_arguments = df_arguments
-        self.df_chunks = df_chunks
-        self.df_edges = None
-        self.df_nodes = None
-        
-    def get_attacks(self):
-        """Get all possible pairs of arguments that attack each other.
-        """
-        pass
     
-    def get_attack_weights(self):
-        """Get weights (and directions) of attacks.
+    def select_by_topic(self, topic:int):
+        """Get arguments that cover a given topic.
         """
-        pass
+        index = self.df_arguments["topics"].apply(lambda x: topic in x)
+        df_selection = self.df_arguments[index]
+        df_selection["argument_id"] = df_selection.index
+        df_selection = df_selection.reset_index(drop=True)
+        return df_selection
     
-    def get_edge_table(self):
-        """Get table of edges and weights.
+    def get_edge_table(self, df_selection):
+        """Given a selection of arguments, get the edge table out of it.
+        """
+        comb_rows = list(itertools.combinations(df_selection.index, 2))
+        combs = [df_selection.loc[c, :][["score", "coherence"]].diff().iloc[1] for c in comb_rows]
+        df_edges = pd.DataFrame(combs)
+        df_edges[["source", "target"]] = comb_rows
+        df_edges = df_edges[df_edges["score"] != 0] # only argumets with different scores 
+        df_edges = df_edges.drop("score", axis=1)
+        df_edges = df_edges.rename(columns={"coherence": "weight"})
+       
+        # weight is computed as coherence_target - coherence_source by diff() above
+        # so positive weight means misorder of source and target. 
+        def detect_direction(row):
+            if row["weight"] > 0:
+                temp = row["source"]
+                row["source"] = row["target"]
+                row["target"] = temp
+            return row
         
-        Attack exists if two arguments sharing at least one topic, but with
-        different argument score. 
-        """
-        if self.df_edges:
-            return
-    
-    def get_node_table(self):
-        """Get table of nodes and labels (supportive/defeated).
-        """
-        if self.df_nodes:
-            return
-
-
-class ArgumentMinerOld:
-    """
-    Accept a json file of arguments and scores as input and create an attacking network.
-
-    Attributes:
-        df_arguments: a pandas DataFrame that contains the arguments and corresponding metadata.
-        nlp_pipe: a spacy pip for natual language processing.
-        wv_model: a word vector model of gensim.
-        tokens: a numpy array that contains all tokens in the argument set.
-        cluster_labels: a numpy array of token cluster labels, thus with the same size as tokens.
-        df_edge: edge table of the attacking network.
-        df_node: node table of the attacking network.
-    """
-
-    df_arguments = None
-    wv_model = None
-    tokens = None
-    cluster_labels = None
-    df_edge = None
-    df_node = None
-
-    def __init__(self, df: pd.DataFrame = None, lang: str ='en'):
-        lang2name = {'en': 'word2vec-google-news-300'}
-        model_name = lang2name[lang]
+        df_edges = df_edges.apply(detect_direction, axis=1)
+        df_edges["weight"] = df_edges["weight"].abs()
+        df_edges = df_edges.reset_index(drop=True)
         
-        self.df_arguments = df
-        self.wv_model = api.load(model_name)
-            
-    def __compute_all_tokens(self):
-        """
-        Compute the full list of tokens in the arguments
-        """
-        if self.tokens is not None and self.tokens.size > 0:
-            return
-
-        self.tokens = []
-        for doc in list(self.df_arguments["ranks"]):
-            doc = eval(doc) # parse str to list
-            for token in doc:
-                token = token[0]
-                if token:
-                    self.tokens.append(token)
-        self.tokens = np.array(self.tokens)
-
-    def __get_token_distances(self) -> np.ndarray:
-        """
-        Given a list of tokens, compute word mover's distance of all possible token pairs
-        """
-        assert self.tokens.size, "Should call compute_all_tokens first!"
-        token_pairs = list(combinations([t.split(" ") for t in self.tokens], 2))
-        token_dists = list(starmap(self.wv_model.wmdistance, token_pairs))
-        token_dists = np.nan_to_num(token_dists, nan=0, posinf=100)
-
-        return token_dists
-
-    def __get_token_distance_matrix(self) -> pd.DataFrame:
-        """
-        Create token distance matrix
-        """
-        dist_matrix = np.zeros((len(self.tokens), len(self.tokens)))
-        dist_matrix[np.triu_indices(len(self.tokens), 1)] = self.__get_token_distances()
-        dist_matrix = dist_matrix + dist_matrix.T
-        dist_matrix = pd.DataFrame(dist_matrix, index=self.tokens, columns=self.tokens)
-
-        return dist_matrix
-
-    @staticmethod
-    def __cluster(dist_matrix: pd.DataFrame, k: int) -> Tuple[float, list]:
-        """
-        cluster the items involved in dist_matrix in k parts.
-        """
-        cluster = KMeans(n_clusters=k, random_state=10)
-        labels = cluster.fit_predict(dist_matrix)
-        try:
-            silhouette = silhouette_score(dist_matrix, labels)
-        except:
-            silhouette = 0
-
-        return silhouette, labels
-
-    def __compute_cluster_labels(self):
-        """
-        Compute clusters of tokens
-        """
-        if self.cluster_labels is not None and self.cluster_labels.size:
-            return
-
-        dist_matrix = self.__get_token_distance_matrix()
-        silhouette_target = -float("inf")
-        for i in range(min(dist_matrix.index.size, 10)):
-            silhouette, labels = self.__cluster(dist_matrix, i + 1)
-            if silhouette > silhouette_target:
-                silhouette_target = silhouette
-                self.cluster_labels = labels
-
-    def __get_cluster_set(self, query: np.ndarray) -> set[int]:
-        """
-        Find the cluster labels of all tokens in a token list and return it as a set
-        """
-        indices = np.isin(self.tokens, query)
-        clusters = self.cluster_labels[indices]
-
-        return set(clusters.flatten())
-
-    def compute_clusters_and_weights(self):
-        """
-        Compute cluster set and weight of each argument.
-        """
-        if {"clusters", "weight"}.issubset(self.df_arguments.columns):
-            return
-
-        self.__compute_all_tokens()
-        self.__compute_cluster_labels()
-
-        clusters = []
-        weights = []
-        for i, row in self.df_arguments.iterrows():
-            ranks = eval(row['ranks'])
-            query = np.array([t[0] for t in ranks])
-            cluster_set = self.__get_cluster_set(query)
-            weight = max([t[1] for t in ranks]) * row["readability"]
-            clusters.append(cluster_set)
-            weights.append(weight)
-        self.df_arguments.loc[self.df_arguments.index, "clusters"] = clusters
-        self.df_arguments.loc[self.df_arguments.index, "weight"] = weights
+        # refill source and target with argument id instead of df index
+        df_edges["source"] = df_selection.loc[df_edges["source"]]["argument_id"]\
+            .reset_index(drop=True)
+        df_edges["target"] = df_selection.loc[df_edges["target"]]["argument_id"]\
+            .reset_index(drop=True)
+        return df_edges
         
-
-    @staticmethod
-    def __get_attacks(
-        group_1: pd.DataFrame, group_2: pd.DataFrame
-    ) -> Tuple[list, list, list]:
+    def get_node_table(self, df_edges, df_nodes):
+        """Given a edge table, get the node table out of it.
         """
-        Given two group of arguments, get all attacks and corresponding weights
-        """
-        source = []
-        target = []
-        weight = []
-        for i_1, row_1 in group_1.iterrows():
-            for i_2, row_2 in group_2.iterrows():
-                if (
-                    row_1["clusters"] and row_2["clusters"]
-                ):  # arguments having tokens from same cluster
-                    if row_1["weight"] > row_2["weight"]:  # attacking direction
-                        source.append(i_1)
-                        target.append(i_2)
-                        weight.append(row_1["weight"])
-                    else:
-                        source.append(i_2)
-                        target.append(i_1)
-                        weight.append(row_2["weight"])
-
-        return source, target, weight
-
-    # TODO: remove instance variable df_edge and df_node, but just return values
-    def compute_edge_table(self):
-        """
-        Compute the edge table of the attacking network.
-        """
-        if self.df_edge:
-            return
-        else: 
-            self.df_edge = {"source": [], "target": [], "weight": []}
+        df_target = df_edges.groupby(by="target", as_index=False).agg({
+            "source": lambda x: list(x)
+        })
         
-        for curr_group in range(1, 5):
-            group_1 = self.df_arguments[self.df_arguments["score"] == curr_group]
-            group_2 = self.df_arguments[self.df_arguments["score"] > curr_group]
-            if group_1.size == 0 or group_2.size == 0:
-                continue
-            temp_source, temp_target, temp_weight = self.__get_attacks(group_1, group_2)
-            self.df_edge["source"] += temp_source
-            self.df_edge["target"] += temp_target
-            self.df_edge["weight"] += temp_weight
-        self.df_edge = pd.DataFrame(self.df_edge)
+        # label arguments as attacking targets
+        def label_target(row):
+            sources = row["source"]
+            try:
+                true_count = df_target["target"].isin(sources).value_counts()[True]
+            except:
+                true_count = 0
+            return "supportive" if true_count == len(sources) else "defeated"
+        df_target["label"] = df_target.apply(label_target, axis=1)
+        df_target = df_target.set_index("target")
         
-        w_min, w_max = self.df_edge['weight'].min(), self.df_edge['weight'].max()
-        if w_min == w_max:
-            self.df_edge.assign(weight=1.0)
-        else:
-            self.df_edge['weight'] = (self.df_edge['weight'] - w_min) / (w_max - w_min)
-    
-    def compute_node_table(self):
-        """Compute node table of the attacking network.
-        
-        label can be 'supportive' or 'defeated'.
-        """
-        if self.df_node:
-            return
-        else:
-            self.df_node = {'argument': [], 'score': [], 'label': []}
-        
-        targets = set(self.df_edge['target'].tolist())
-        for i, row in self.df_arguments.iterrows():
-            attackers = self.df_edge[self.df_edge['target'] == i]['source']
-            attackers = set(attackers.tolist())
-            self.df_node['argument'].append(row['argument']) 
-            self.df_node['score'].append(row['score'])
-             
-            if not attackers or attackers & targets == attackers:
-                self.df_node['label'].append('supportive')
-            else:
-                self.df_node['label'].append('defeated')
-                
-        self.df_node = pd.DataFrame(self.df_node)
+        # assign labels to arguments
+        def assign_label(row):
+            id = row["argument_id"]
+            try:
+                return df_target.loc[id]["label"]
+            except:
+                return "supportive"
+        df_nodes["label"] = df_nodes.apply(assign_label, axis=1)
+        return df_nodes
